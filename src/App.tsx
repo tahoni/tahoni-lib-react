6d4fca36be5213e0b2f7e9252489dd83ce3b56bb
Leoni Lubbinge <tahoni@gmail.com>
2024-07-22 13:52:10 +0200

    Add index.ts to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {Breakpoints} from "../lib";
 import {MaskOverlay} from "../lib";
 import {Loader} from "../lib";
-import {ExampleSlider} from "./components/ExampleSlider/ExampleSlider.tsx";
+import {ExampleSlider} from "./components/ExampleSlider";
 import './App.scss'
 
 function App() {
