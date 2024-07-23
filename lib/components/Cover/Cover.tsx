2c28531d0d61a202ef2b72aab11702635e2aab44
Leoni Lubbinge <tahoni@gmail.com>
2024-07-23 22:50:23 +0200

    Use Bootstrap row and column again, while still staying centered

--- lib/components/Cover/Cover.tsx
+++ lib/components/Cover/Cover.tsx
@@ -1,13 +1,17 @@
 import React, {ReactElement} from "react";
-import {Container, Image} from "react-bootstrap";
+import {Col, Container, Image, Row} from "react-bootstrap";
 import "./Cover.scss";
 
 export const Cover = (props: CoverProps): ReactElement => {
     return (
         <Container fluid className="cover">
+            <Row>
+                <Col>
                     <Image fluid src={props.slide?.image}
                            alt={props.slide?.description}/>
                     <h1 className="coverText">{props.slide?.text}</h1>
+                </Col>
+            </Row>
         </Container>
     )
 }
