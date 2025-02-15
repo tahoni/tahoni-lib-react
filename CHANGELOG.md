b849a4ceb12f48b51629517122a05c79bc0203f9
Leoni Lubbinge <tahoni@gmail.com>
2025-02-15 14:31:46 +0200

    Remove a setting from `vite.config.ts` to roll back the changes to build the project with smaller chunks.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,9 @@
 ### [Version 3.2.3](https://github.com/tahoni/template-react/releases/tag/version-3.2.2) - _2025-02-15_
 Mitigated a moderate severity security vulnerability.<br/>
 
+#### General Technical Changes
+- A setting was removed from `vite.config.ts` to roll back the changes to build the project with smaller chunks.
+
 #### Dependencies
 - Updated the `esbuild` dependency to mitigate a moderate security vulnerability.
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import { extname, relative, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
+import { resolve } from "node:path";
 
 import { peerDependencies } from "./package.json";
 
@@ -33,22 +31,8 @@ export default defineConfig({
       external: [
         "react",
         "react/jsx-runtime",
-        ...Object.keys(peerDependencies),
+        ...Object.keys(peerDependencies)
       ],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx,scss}", {
-            ignore: ["lib/!**/!*.d.ts", "lib/vite-env.d.ts"],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ]),
-      ),
       output: {
         assetFileNames: "[name][extname]",
         entryFileNames: "[name].js",
